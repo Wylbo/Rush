bb7c315830d9f719e4bd27ac30b97f98a56fbab9
Maximilien Galea <m.galea@student.isartdigital.com>
2019-10-21 12:57:51 +0200

    first commit, prefab placeholder

---
+++ Assets/Rush/Scripts/Move.cs
@@ -0,0 +1,19 @@
+///-----------------------------------------------------------------
+/// Author : Maximilien Galea
+/// Date : 21/10/2019 12:56
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.IsartDigital.Rush {
+	public class Move : MonoBehaviour {
+	
+		private void Start () {
+			
+		}
+		
+		private void Update () {
+			
+		}
+	}
+}
